c236ced4d1c7ad60b1bcc74bb3865540ec4d2dca
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-24 19:47:22 -0600

    Add project files.

---
+++ Intro2/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Intro2
+{
+    public partial class MainPage : TabbedPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
---
+++ Intro2/pages/Pestana1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Intro2.pages;
+
+public partial class Pestana1 : ContentPage
+{
+	public Pestana1()
+	{
+		InitializeComponent();
+	}
+}
