a7f20e3bcec40633a74a155c653e62cf69918e2c
CordzB <167384843+CordzB@users.noreply.github.com>
2024-11-24 21:58:05 -0600

    AGREGANDO LAS ALERTAS Y FUNCIONES DE SUMA

---
+++ Intro2/pages/Pestana2.xaml.cs
@@ -0,0 +1,9 @@
+namespace Intro2.pages;
+
+public partial class Pestana2 : ContentPage
+{
+	public Pestana2()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Intro2/pages/Pestana3.xaml.cs
@@ -0,0 +1,9 @@
+namespace Intro2.pages;
+
+public partial class Pestana3 : ContentPage
+{
+	public Pestana3()
+	{
+		InitializeComponent();
+	}
+}
